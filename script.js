ece4b79a7dcf391cd9dcf567f8f4fb9838067895
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-02-07 23:58:20 +0200

    Add announcement message at end of game

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const playerFactory = () => {
   const wins = 0;
-  const losses = 0;
+
   const updateScore = () => { };
 
   return { updateScore };
 };
-
-const playerOne = playerFactory();
-const playerTwo = playerFactory();
 
 const gameBoard = (() => {
   const gameArray = [];
@@ -29,8 +26,11 @@ const gameBoard = (() => {
 
 const gameFlow = (() => {
   let currentTurn = "";
+  let isGameOver = false;
 
-  const startGame = () => { };
+  const restartGame = () => {
+    isGameOver = false;
+  };
 
   const determineTurn = (isBoardEmpty) => {
     if (isBoardEmpty || currentTurn === "Player 2") {
@@ -63,57 +63,93 @@ const gameFlow = (() => {
         currentGameArray[index + 6],
       ]);
     }
-
     const gameArrayAll = [
       ...gameArrayRows,
       ...gameArrayCols,
       ...gameArrayDiagonals,
     ];
-
     // check for wins
     for (let index = 0; index < gameArrayAll.length; index++) {
       const gameArraySequence = gameArrayAll[index].join("");
-      if (gameArraySequence === winnerX)
+      if (gameArraySequence === winnerX) {
         displayController.announceOutcome("Player 1");
-      else if (gameArraySequence === winnerO)
+        isGameOver = true;
+      } else if (gameArraySequence === winnerO) {
         displayController.announceOutcome("Player 2");
+        isGameOver = true;
+      }
     }
-
     // check for draws
     const isArrayFullyPopulated = (item) => item !== "";
     if (currentGameArray.every(isArrayFullyPopulated)) {
       displayController.announceOutcome("Draw");
+      isGameOver = true;
     }
+    return isGameOver;
   };
 
-  return { determineTurn, determineOutcome, startGame };
+  return { determineTurn, determineOutcome, restartGame };
 })();
 
 const displayController = (() => {
+  const startGameOverlay = document.querySelector(".overlay");
   const gameCells = document.querySelectorAll(".game-cell");
+  // announcements
+  const announcementContainer = document.querySelector(
+    ".announcement-container"
+  );
+  const announcementMessage = document.querySelector(".announcement");
+  // buttons
+  const selectPlayerButton = document.querySelector("#select-player");
+  const selectAIButton = document.querySelector("#select-AI");
+  const clearBtn = document.querySelector(".clear");
   const restartBtn = document.querySelector(".restart");
-  const announcementBoard = document.querySelector(".announcement-board");
+  const startGameButton = document.querySelectorAll("#start-game");
+  // player popups and names
+  const playerVsPlayerPopup = document.querySelector("#player-vs-player");
+  const playerVsAIPopup = document.querySelector("#player-vs-AI");
+  const playerOneName = document.querySelector(".player-one-name");
+  const playerTwoName = document.querySelector(".player-two-name");
+  // flags
   let isBoardEmpty = true;
+  let isGameVsAI = false;
 
-  // TODO: disable any clicks after clear is called....
+  const clearPrompts = () => {
+    announcementMessage.textContent = "";
+  };
+
   const clearDisplayAndGameboard = () => {
     gameCells.forEach((cell) => {
       cell.textContent = "";
     });
     gameBoard.clearGameArray();
     isBoardEmpty = true;
-    setTimeout(clearPrompts, 1000);
+    gameFlow.restartGame();
+    announcementContainer.classList.remove("announcement-overlay");
+    clearPrompts();
   };
 
-  // TODO: update below to clear scoreboard (or not depening on clear or restart game selected....)
-  const clearPrompts = () => {
-    announcementBoard.textContent = "";
+  const restartGame = () => {
+    gameCells.forEach((cell) => {
+      cell.textContent = "";
+    });
+    gameBoard.clearGameArray();
+    isBoardEmpty = true;
+    gameFlow.restartGame();
+    clearPrompts();
+    startGameOverlay.classList.toggle("hide-elements");
+    playerVsAIPopup.classList.remove("hide-elements");
+    playerVsAIPopup.classList.remove("unhide-elements");
+    playerVsPlayerPopup.classList.remove("hide-elements");
+    playerVsPlayerPopup.classList.remove("unhide-elements");
+    playerVsPlayerPopup.reset();
+    playerVsAIPopup.reset();
   };
 
   const populateDisplay = (event) => {
     const cellIndex = Number(event.target.dataset.index);
     const cellValue = event.target.textContent;
-    if (cellValue !== "") return;
+    if (cellValue !== "" || gameFlow.determineOutcome()) return;
     const currentTurn = gameFlow.determineTurn(isBoardEmpty);
     if (currentTurn === "Player 1" && cellValue === "") {
       event.target.textContent = "X";
@@ -127,18 +163,74 @@ const displayController = (() => {
     }
   };
 
-  const announceOutcome = (outcome) => {
-    announcementBoard.textContent =
-      outcome === "Draw" ? "Draw!" : `${outcome} wins!`;
-    setTimeout(clearDisplayAndGameboard, 2.0 * 1000);
+  const openPlayerVsPlayerPopup = () => {
+    if (playerVsAIPopup.classList.contains("unhide-elements")) {
+      playerVsAIPopup.classList.remove("unhide-elements");
+    }
+    playerVsPlayerPopup.classList.add("unhide-elements");
+    isGameVsAI = false;
   };
+
+  const openPlayerVsAIPopup = () => {
+    if (playerVsPlayerPopup.classList.contains("unhide-elements")) {
+      playerVsPlayerPopup.classList.remove("unhide-elements");
+    }
+    playerVsAIPopup.classList.add("unhide-elements");
+    isGameVsAI = true;
+  };
+
+  const startGame = (e) => {
+    const playerOneNamePvP =
+      playerVsPlayerPopup.children[0].children.player1.value;
+    const playerTwoNamePVP =
+      playerVsPlayerPopup.children[0].children.player2.value;
+    const playerOneNameAI = playerVsAIPopup.children[0].children.player1.value;
+
+    if (!isGameVsAI && playerOneNamePvP !== "" && playerTwoNamePVP !== "") {
+      e.preventDefault();
+      startGameOverlay.classList.toggle("hide-elements");
+      playerVsPlayerPopup.classList.toggle("hide-elements");
+      playerOneName.textContent = playerOneNamePvP;
+      playerTwoName.textContent = playerTwoNamePVP;
+    } else if (isGameVsAI && playerOneNameAI !== "") {
+      e.preventDefault();
+      startGameOverlay.classList.toggle("hide-elements");
+      playerVsAIPopup.classList.toggle("hide-elements");
+      playerOneName.textContent = playerOneNameAI;
+      playerTwoName.textContent = "Computer";
+    }
+  };
+
+  const announceOutcome = (outcome) => {
+    announcementContainer.classList.add("announcement-overlay");
+    announcementMessage.classList.add("announcement-message");
+    const winningPlayer =
+      outcome === "Player 1"
+        ? playerOneName.textContent
+        : playerTwoName.textContent;
+    announcementMessage.textContent =
+      outcome === "Draw" ? "Draw!" : `${winningPlayer} wins!`;
+    setTimeout(clearDisplayAndGameboard, 2000);
+  };
+
+  // select Player or AI
+  selectPlayerButton.addEventListener("click", openPlayerVsPlayerPopup);
+  selectAIButton.addEventListener("click", openPlayerVsAIPopup);
+
+  // start game
+  startGameButton.forEach((button) => {
+    button.addEventListener("click", startGame);
+  });
 
   // add marks
   gameCells.forEach((cell) => {
     cell.addEventListener("click", populateDisplay);
   });
   // clear display
-  restartBtn.addEventListener("click", clearDisplayAndGameboard);
+  clearBtn.addEventListener("click", clearDisplayAndGameboard);
+
+  // restart game
+  restartBtn.addEventListener("click", restartGame);
 
   return {
     clearDisplayAndGameboard,
